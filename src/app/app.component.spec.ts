8a9c9a2e92ca2864cb08dcae5a5bcee07fb1457e
Angel J. Sechar Valdez <asv_18_1999@hotmail.com>
2025-08-10 19:48:12 -0500

    .eslintrc.json

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,2 +1,2 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+// import { TestBed } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
