f5228681ef9a56bc1b13069eb6d2e11282506a90
Angel J. Sechar Valdez <asv_18_1999@hotmail.com>
2025-09-15 18:23:13 -0500

    config modern fetch and auto binding for components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
     provideClientHydration(),
     provideAnimationsAsync(),
     provideServiceWorker('ngsw-worker.js', {
